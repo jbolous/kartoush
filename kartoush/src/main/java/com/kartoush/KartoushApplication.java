5b366841a3580f85ac8ecbe03dd85a2251b29efe
Jack B <jbolous@gmail.com>
2026-02-11 11:17:24 -0600

    Merge pull request #72 from jbolous/chore/bootstrap-spring-boot
    Bootstrap Spring Boot project with Gradle

---
+++ kartoush/src/main/java/com/kartoush/KartoushApplication.java
@@ -0,0 +1,13 @@
+package com.kartoush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartoushApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(KartoushApplication.class, args);
+	}
+
+}
