c998e0627991efe1c9eede25b3952dfcf4b6c550
Jack Bolous <jbolous@gmail.com>
2026-04-16 22:36:19 -0500

    feat(testing): add rest assured integration test support

---
+++ test-support/src/main/java/com/kartoush/testsupport/HttpSpringIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.kartoush.testsupport;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@IntegrationTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public @interface HttpSpringIntegrationTest {}
